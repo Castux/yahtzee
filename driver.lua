002343edd076e203f2c7b8dbf2d0e1526c262400
Noé Falzon <noe@yousician.com>
2016-06-30 12:39:39 +0300

    Fixed driver after switch to box set indices

--- driver.lua
+++ driver.lua
@@ -8,36 +8,29 @@ local function run(num_boxes, start, skip)
 	
 	local box_sets = utils.subsets(yahtzee.boxes)
 
-	-- sort box sets by count
+	-- sort box sets by count and load/check existence
 
 	local counts = {}
+	local values = {}
 	
-	for i = 0, yahtzee.box_set_builder.max_index do
+	for i = 1, yahtzee.box_set_builder.max_index do
 		
 		local list = yahtzee.box_set_list(i)
 		
 		counts[#list] = counts[#list] or {}
 		table.insert(counts[#list], i)
-	end
-
-	-- load relevant previous results
-
-	local values = {}
-	
-	for i = 1,yahtzee.box_set_builder.max_index do
 		
-		if i == num_boxes then
+		if #list == num_boxes then
 			values[i] = utils.load("values_" .. i, true)
 			if values[i] then
 				print("Skipped " .. i)
 			end
-		elseif i == num_boxes - 1 then
+		elseif #list == num_boxes - 1 then
 			values[i] = utils.load("values_" .. i)
 			if values[i] then
 				print("Loaded " .. i)
 			end
 		end
-		
 	end
 	
 	-- compute
@@ -60,4 +53,11 @@ end
 -- Read command line
 
 a,b,c = ...
-run(tonumber(a),tonumber(b),tonumber(c))
+
+if not a or not b or not c then
+	for i = 1,13 do
+		run(i,0,1)
+	end
+else
+	run(tonumber(a),tonumber(b),tonumber(c))
+end
