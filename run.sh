732099a56104583e731a228548d14df28e9d4f5a
Noé Falzon <noe@yousician.com>
2016-06-30 10:20:47 +0300

    Made run script handle 6 workers and loop automatically to higher levels

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-LEVEL=3
-luajit driver.lua $LEVEL 0 4 & luajit driver.lua $LEVEL 1 4 & luajit driver.lua $LEVEL 2 4 & luajit driver.lua $LEVEL 3 4
+
+for i in `seq 1 13`;
+do
+	echo Starting level $i
+
+	luajit driver.lua $i 0 6 &
+	luajit driver.lua $i 1 6 &
+	luajit driver.lua $i 2 6 &
+	luajit driver.lua $i 3 6 &
+	luajit driver.lua $i 4 6 &
+	luajit driver.lua $i 5 6 &
+
+	wait
+	echo All done for level $i
+done
