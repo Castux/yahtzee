721c3c3588cdf7e3be1f59513ce36e7b6696daa1
Noé Falzon <noe@yousician.com>
2016-07-07 11:51:00 +0300

    Rollboxes

--- www/script.js
+++ www/script.js
@@ -2,10 +2,14 @@ var inputs;
 var upper_total_p;
 var total_p;
 var boxset_p;
+var rollboxes;
+var actionboxes;
 
 var upper_total = 0;
 var total = 0;
 var boxset = 0;
+var step = 0;
+var round = 0;
 
 function init()
 {
@@ -20,6 +24,18 @@ function init()
 	total_p = document.getElementById("total");
 	boxset_p = document.getElementById("boxset");
 
+	rollboxes = document.getElementsByClassName("rollbox");
+	for (var i = 0; i < rollboxes.length; i++)
+	{
+		let j = i;
+		rollboxes[i].onchange = function()
+		{
+			onRollboxChanged(j);
+		}
+	};
+
+	actionboxes = document.getElementsByClassName("actionbox");
+
 	onScoresChanged();
 }
 
@@ -27,6 +43,7 @@ function onScoresChanged()
 {
 	upper_total = 0;
 	total = 0;
+	round = 0;
 
 	// Up
 
@@ -40,6 +57,8 @@ function onScoresChanged()
 
 			if(i < 6)
 				upper_total += number;
+
+			round++;
 		}
 	};
 
@@ -56,6 +75,9 @@ function onScoresChanged()
 	total_p.innerHTML = "Total: " + total;
 
 	computeBoxset();
+
+	var step_p = document.getElementById("step");
+	step_p.innerHTML = "Round " + (round + 1);
 }
 
 function computeBoxset()
@@ -72,6 +94,37 @@ function computeBoxset()
 	}
 
 	boxset_p.innerHTML = "Boxset: " + boxset;
+}
+
+function onRollboxChanged(index)
+{
+	var str = rollboxes[index].value.split("");
+
+	if(str.length != 5)
+	{
+		rollboxes[index].value = "";
+		return;
+	}
+	
+	for (var i = 0; i < str.length; i++)
+	{
+		var num = parseInt(str[i]);
+		if(num < 0 || num > 6)
+		{
+			rollboxes[index].value = "";
+			return;
+		}
+
+		str[i] = num;
+	};
+
+	str.sort();
+	str = str.join("");
+	rollboxes[index].value = str;
+
+	// trigger data fetch
+
+	actionboxes[index].innerHTML = "YAAA " + str;
 }
 
 init();
